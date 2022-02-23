ccd27e1609fd759175a9adc88f9352f6505a3423
tluke97 <39325865+tluke97@users.noreply.github.com>
2022-02-22 23:04:55 -0600

    initializing repo

---
+++ PewPew/Objects/PewPewLaserView.swift
@@ -0,0 +1,20 @@
+//
+//  PewPewLaserView.swift
+//  PewPew
+//
+//  Created by Tanner Luke on 2/10/22.
+//
+
+import UIKit
+
+class PewPewLaserView: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ PewPew/Objects/PewPewSubview.swift
@@ -0,0 +1,20 @@
+//
+//  PewPewSubview.swift
+//  PewPew
+//
+//  Created by Tanner Luke on 2/11/22.
+//
+
+import UIKit
+
+class PewPewSubview: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
